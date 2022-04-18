a6e90a4c4a33c82218540991e22e3aa02468fb7a
NoWorryYun <dbstjd3201@naver.com>
2022-04-18 17:17:56 +0900

    2022-04-18 반복문 1차

---
+++ src/com/javaex/ex04/Ex08.java
@@ -0,0 +1,20 @@
+package com.javaex.ex04;
+
+public class Ex08 {
+
+	public static void main(String[] args) {
+
+		int i = 1;
+		
+		while(true) {
+				//조건이 되면 탈출
+			if(i % 6 == 0 && i % 14 == 0) {
+				//if문이 true
+				System.out.println(i);
+				break;
+			}
+			//while 안에 있기 때문에 else를 따로 안써줘도 가능 > 특수한 경우임
+			i++;
+		}
+	}
+}
---
+++ src/com/javaex/ex04/Ex09.java
@@ -0,0 +1,30 @@
+package com.javaex.ex04;
+
+import java.util.Scanner;
+
+public class Ex09 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			System.out.println("숫자를 입력하세요");
+			int x = sc.nextInt();
+			if(x == 0) {
+				System.out.println("종료");
+				break;
+			}else {
+				if(x % 3 == 0) {
+					System.out.println("3의 배수입니다.");
+				}else{
+					System.out.println("3의 배수가 아닙니다.");
+				}
+			}
+		}
+		sc.close();
+	}
+}
+
+
